5e4849abea4139d8a708bdb2ce51ec39c8136d58
Hanna Nyblom <hannanyblom@Hannahs-MacBook-Pro-2.local>
2023-09-07 12:17:38 -0400

    rate limit requests

--- run_request.py
+++ run_request.py
@@ -1,1 +1,11 @@
 print("Github Users")
+
+import requests
+import json
+
+access_point = "https://api.github.com"
+print (access_point)
+
+response_text = requests.get(access_point + "/rate_limit").text
+
+print(json.loads(response_text))
